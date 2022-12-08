b5a3ebad748507bfb86c918f70973927165c4599
acehinnnqru <acehinnnqru@gmail.com>
2022-12-09 03:19:31 +0800

    feat: day4 done.

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,20 @@ use aoc_2022::{
     solutions::{
         day1::{Day1Part1, Day1Part2},
         day2::{Day2Part1, Day2Part2},
-        day3::{Day3Part1, Day3Part2},
+        day3::{Day3Part1, Day3Part2}, day4::{Day4Part1, Day4Part2},
     },
 };
 
 fn main() {
-    day1();
-    day2();
-    day3();
+    day_n(1, &Day1Part1 {}, &Day1Part2 {});
+    day_n(2, &Day2Part1 {}, &Day2Part2 {});
+    day_n(3, &Day3Part1 {}, &Day3Part2 {});
+    day_n(4, &Day4Part1 {}, &Day4Part2 {});
 }
 
-fn day1() {
-    let input = fs::read_to_string("data/day1.txt").expect("read failed");
-    let s = Day1Part1 {};
-    s.run(&input);
-    let s = Day1Part2 {};
-    s.run(&input);
+fn day_n(n: i32, part1: &dyn Solution, part2: &dyn Solution) {
+    let input = fs::read_to_string(format!("data/day{}.txt", n)).expect("read failed");
+    part1.run(&input);
+    part2.run(&input);
 }
 
-fn day2() {
-    let input = fs::read_to_string("data/day2.txt").expect("read failed");
-    let s = Day2Part1 {};
-    s.run(&input);
-    let s = Day2Part2 {};
-    s.run(&input);
-}
-
-fn day3() {
-    let input = fs::read_to_string("data/day3.txt").expect("read failed");
-    let s = Day3Part1 {};
-    s.run(&input);
-    let s = Day3Part2 {};
-    s.run(&input);
-}
---
+++ src/solutions/day4.rs
@@ -0,0 +1,52 @@
+use crate::solution::Solution;
+
+pub struct Day4Part1 {}
+
+fn to_interval(s: &str) -> (i32, i32) {
+    let interval: Vec<&str> = s.split('-').collect();
+    let min: i32 = interval[0].parse().unwrap();
+    let max: i32 = interval[1].parse().unwrap();
+
+    (min, max)
+}
+
+fn split2intervals(line: &String) -> (i32, i32, i32, i32) {
+    let splits: Vec<&str> = line.split(',').collect();
+    let (left_min, left_max) = to_interval(splits[0]);
+    let (right_min, right_max) = to_interval(splits[1]);
+
+    (left_min, left_max, right_min, right_max)
+}
+
+fn be_fully_contained(line: &String) -> bool {
+    let (lmin, lmax, rmin, rmax) = split2intervals(line);
+    if (lmin <= rmin && lmax >= rmax) || (rmin <= lmin && rmax >= lmax) {
+        return true;
+    }
+
+    false
+}
+
+fn be_overlap(line: &String) -> bool {
+    let (lmin, lmax, rmin, rmax) = split2intervals(line);
+    if (lmax < rmin) || (rmax < lmin) {
+        return false;
+    }
+    true
+}
+
+impl Solution for Day4Part1 {
+    fn run(&self, input: &String) {
+        let count = input.lines().map(|x| be_fully_contained(&String::from(x))).filter(|x| *x).count();
+        println!("result of day 4 part 1 is: {}", count);
+    }
+}
+
+pub struct Day4Part2 {}
+
+impl Solution for Day4Part2 {
+    fn run(&self, input: &String) {
+        let count = input.lines().map(|x| be_overlap(&String::from(x))).filter(|x| *x).count();
+        println!("result of day 4 part 2 is: {}", count);
+    }
+}
