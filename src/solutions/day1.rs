ba7fd4d73dac621cf583a5ed5828d82b90db040a
acehinnnqru <acehinnnqru@gmail.com>
2022-12-09 10:10:34 +0800

    feat: day1 optimized

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -4,25 +4,27 @@ use crate::solution::Solution;
 
 pub struct Day1Part1 {}
 
-fn cal_max_n(lines: Lines, n: usize) -> Vec<i32> {
-    let mut candidates = vec![0; n];
-    let iter = lines.into_iter();
+fn update_candidates(candidates: &mut Vec<i32>, cur: i32) {
+    for i in 0..candidates.len() {
+        if cur > candidates[i] {
+            candidates[i] = cur;
+            return;
+        }
+    }
+}
 
+fn cal_max_n(lines: Lines, n: usize) -> Vec<i32> {
     let mut cur = 0;
-    for item in iter {
-        if !item.is_empty() {
-            cur += item.parse::<i32>().unwrap();
-            continue;
-        } else {
-            for i in 0..candidates.len() {
-                if cur > candidates[i] {
-                    candidates[i] = cur;
-                    break;
-                }
-            }
-            cur = 0;
+    let mut candidates = vec![0; n];
+    for line in lines {
+        if !line.is_empty() {
+            cur += line.parse::<i32>().unwrap();
             continue;
         }
+        
+        update_candidates(&mut candidates, cur);
+        cur = 0;
+        continue;
     }
 
     return candidates;
