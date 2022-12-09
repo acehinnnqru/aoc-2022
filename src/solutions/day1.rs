6359cec57ef11b154ea0ff72396e21472f433daf
acehinnnqru <acehinnnqru@gmail.com>
2022-12-09 12:30:24 +0800

    feat: optimize day 1

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -1,38 +1,59 @@
-use std::str::Lines;
+use std::{cmp::Reverse, collections::BinaryHeap, str::Lines};
 
 use crate::solution::Solution;
 
 pub struct Day1Part1 {}
 
-fn update_candidates(candidates: &mut Vec<i32>, cur: i32) {
-    for i in 0..candidates.len() {
-        if cur > candidates[i] {
-            candidates[i] = cur;
+struct Candidates {
+    h: BinaryHeap<Reverse<i32>>,
+    limit: usize,
+}
+
+impl Candidates {
+    fn new(n: usize) -> Candidates {
+        Candidates {
+            h: BinaryHeap::new(),
+            limit: n,
+        }
+    }
+
+    fn update(&mut self, cur: i32) {
+        if self.h.len() < self.limit {
+            self.h.push(Reverse(cur));
             return;
         }
+
+        if self.h.peek().unwrap() < &Reverse(cur) {
+            self.h.pop();
+            self.h.push(Reverse(cur));
+        }
+    }
+
+    fn sum(self) -> i32 {
+        self.h.into_iter().map(|Reverse(x)| x).sum()
     }
 }
 
-fn cal_max_n(lines: Lines, n: usize) -> Vec<i32> {
+fn cal_max_n(lines: Lines, n: usize) -> i32 {
     let mut cur = 0;
-    let mut candidates = vec![0; n];
+    let mut candidates = Candidates::new(n);
     for line in lines {
         if !line.is_empty() {
             cur += line.parse::<i32>().unwrap();
             continue;
         }
-        
-        update_candidates(&mut candidates, cur);
+
+        candidates.update(cur);
         cur = 0;
         continue;
     }
 
-    return candidates;
+    candidates.sum()
 }
 
 impl Solution for Day1Part1 {
     fn run(&self, input: &String) -> i32 {
-        cal_max_n(input.lines(), 1).iter().sum::<i32>()
+        cal_max_n(input.lines(), 1)
     }
 }
 
@@ -40,6 +61,6 @@ pub struct Day1Part2 {}
 
 impl Solution for Day1Part2 {
     fn run(&self, input: &String) -> i32 {
-        cal_max_n(input.lines(), 3).iter().sum::<i32>()
+        cal_max_n(input.lines(), 3)
     }
 }
