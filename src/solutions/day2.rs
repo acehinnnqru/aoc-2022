7013d6fb67077d840fb0bcc2e79d9f89d85c7ef8
acehinnnqru <acehinnnqru@gmail.com>
2022-12-09 15:25:48 +0800

    feat: optimize codes

--- src/solutions/day2.rs
+++ src/solutions/day2.rs
@@ -2,23 +2,6 @@ use std::str::Lines;
 
 use crate::solution::Solution;
 
-pub struct Day2Part1 {}
-
-trait Day2 {
-    fn process(&self, lines: Lines, f: fn(opponent: &u8, me: &u8) -> u8) -> i32 {
-        let mut score = 0;
-        for line in lines {
-            let bs: Vec<u8> = line.bytes().collect();
-            let opponent = bs[0];
-            let me = f(&opponent, &bs[2]);
-
-            score += point(&opponent, &me);
-        }
-
-        score
-    }
-}
-
 fn point(opponent: &u8, me: &u8) -> i32 {
     let choice_point = me - b'A' + 1;
     if opponent == me {
@@ -53,6 +36,23 @@ fn todraw(opponent: &u8) -> u8 {
     *opponent
 }
 
+trait Day2 {
+    fn process(&self, lines: Lines, f: fn(opponent: &u8, me: &u8) -> u8) -> i32 {
+        let mut score = 0;
+        for line in lines {
+            let bs: Vec<u8> = line.bytes().collect();
+            let opponent = bs[0];
+            let me = f(&opponent, &bs[2]);
+
+            score += point(&opponent, &me);
+        }
+
+        score
+    }
+}
+
+pub struct Day2Part1 {}
+
 impl Day2 for Day2Part1 {}
 
 impl Solution for Day2Part1 {
@@ -71,8 +71,6 @@ impl Solution for Day2Part1 {
 }
 
 pub struct Day2Part2 {}
-
-impl Day2Part2 {}
 
 impl Day2 for Day2Part2 {}
 
