a97e500c139d16d49ba4f01ed6d15c8c36047cab
acehinnnqru <acehinnnqru@gmail.com>
2022-12-12 21:28:13 +0800

    feat: impl Monkey, Operation, Tester

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -2,27 +2,32 @@ use std::collections::VecDeque;
 
 enum OperationType {
     Plus,
-    Sub,
     Multi,
-    Divide,
 }
 
 struct Operation {
     ot: OperationType,
     constant: i32,
+    old_itself: bool,
 }
 
 impl Operation {
-    fn new(ot: OperationType, constant: i32) -> Operation {
-        Operation { ot, constant }
+    fn new(ot: OperationType, constant: i32, old_itself: bool) -> Operation {
+        Operation {
+            ot,
+            constant,
+            old_itself,
+        }
     }
 
     fn execute(&self, old: i32) -> i32 {
+        let mut arg = self.constant;
+        if self.old_itself {
+            arg = old;
+        }
         match self.ot {
-            OperationType::Plus => old + self.constant,
-            OperationType::Sub => old - self.constant,
-            OperationType::Multi => old * self.constant,
-            OperationType::Divide => old / self.constant,
+            OperationType::Plus => old + arg,
+            OperationType::Multi => old * arg,
         }
     }
 }
@@ -58,20 +63,34 @@ struct Monkey {
 }
 
 impl Monkey {
-    fn new(items: Vec<i32>, operation: Operation, tester: Tester) -> Monkey {
+    fn new(items: VecDeque<i32>, operation: Operation, tester: Tester) -> Monkey {
         Monkey {
-            items: VecDeque::<i32>::from_iter(items.iter().cloned()),
+            items,
             operation,
             tester,
         }
     }
 
-    fn pop(&self) -> (usize, i32) {
-        unimplemented!()
+    fn pop(&mut self) -> Option<(usize, i32)> {
+        if self.is_empty() {
+            return None;
+        }
+
+        let mut item = self.items.pop_front().unwrap();
+
+        item = self.operation.execute(item);
+        item /= 3;
+        let target = self.tester.test(item);
+
+        Some((target, item))
     }
 
     fn is_empty(&self) -> bool {
         self.items.is_empty()
+    }
+
+    fn push(&mut self, item: i32) {
+        self.items.push_back(item);
     }
 }
 
@@ -94,16 +113,63 @@ impl Game {
 }
 
 fn build_monkey(chunks: &[String]) -> Monkey {
-    unimplemented!()
+    let mut iter = chunks.iter();
+
+    // let index: usize = iter
+    //     .next()
+    //     .unwrap()
+    //     .trim_end_matches(':')
+    //     .trim_start_matches("Monkey")
+    //     .parse()
+    //     .unwrap();
+
+    let items: VecDeque<i32> = iter
+        .next()
+        .unwrap()
+        .trim_start_matches("Starting items:")
+        .trim()
+        .split_terminator(&[',', ' '])
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+
+    let mut op_splits = iter.next().unwrap().split_whitespace().rev();
+    let (old_itself, constant) = match op_splits.next().unwrap() {
+        "old" => (true, 0),
+        d => (false, d.parse().unwrap()),
+    };
+    let ot = match op_splits.next().unwrap() {
+        "*" => OperationType::Multi,
+        "+" => OperationType::Plus,
+        _ => unreachable!(),
+    };
+    let operation = Operation::new(ot, constant, old_itself);
+
+    let mut last_number = || {
+        iter.next()
+            .unwrap()
+            .split_whitespace()
+            .rev()
+            .next()
+            .unwrap()
+            .parse()
+            .unwrap()
+    };
+    let test_arg: i32 = last_number();
+    let ttarget: usize = last_number() as usize;
+    let ftarget: usize = last_number() as usize;
+    let tester = Tester::new(test_arg, ttarget, ftarget);
+
+    Monkey::new(items, operation, tester)
 }
 
 fn build_game(input: &str) -> Game {
     let mut game = Game::new();
     let lines: Vec<String> = input.lines().map(String::from).collect();
-    let chunks = lines.chunks(5);
-    for chunk in chunks {
-        let monkey = build_monkey(chunk);
-        game.push(monkey);
-    }
-    unimplemented!()
+    let chunks = lines.chunks(7);
+
+    chunks.for_each(|chunk| {
+        game.push(build_monkey(chunk));
+    });
+
+    game
 }
