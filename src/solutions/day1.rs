245438ac54858e6e9c6fc10ce0c902fd2b45ac4b
acehinnnqru <acehinnnqru@gmail.com>
2022-12-30 00:29:19 +0800

    feat(day1): optimize names

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -1,50 +1,50 @@
-use std::{cmp::Reverse, collections::BinaryHeap, str::Lines};
+use std::{cmp::Reverse, collections::BinaryHeap, str::Lines, any::Any};
 
 use crate::solution::Solution;
 
 pub struct Day1Part1 {}
 
 struct Candidates {
-    h: BinaryHeap<Reverse<i32>>,
+    max_heap: BinaryHeap<Reverse<i32>>,
     limit: usize,
 }
 
 impl Candidates {
     fn new(n: usize) -> Candidates {
         Candidates {
-            h: BinaryHeap::new(),
+            max_heap: BinaryHeap::new(),
             limit: n,
         }
     }
 
     fn update(&mut self, cur: i32) {
-        if self.h.len() < self.limit {
-            self.h.push(Reverse(cur));
+        if self.max_heap.len() < self.limit {
+            self.max_heap.push(Reverse(cur));
             return;
         }
 
-        if self.h.peek().unwrap() < &Reverse(cur) {
-            self.h.pop();
-            self.h.push(Reverse(cur));
+        if self.max_heap.peek().unwrap() < &Reverse(cur) {
+            self.max_heap.pop();
+            self.max_heap.push(Reverse(cur));
         }
     }
 
     fn sum(self) -> i32 {
-        self.h.into_iter().map(|Reverse(x)| x).sum()
+        self.max_heap.into_iter().map(|Reverse(x)| x).sum()
     }
 }
 
-fn cal_max_n(lines: Lines, n: usize) -> i32 {
-    let mut cur = 0;
+fn sum_of_top_n_candidates(lines: Lines, n: usize) -> i32 {
+    let mut current_count = 0;
     let mut candidates = Candidates::new(n);
     for line in lines {
         if !line.is_empty() {
-            cur += line.parse::<i32>().unwrap();
+            current_count += line.parse::<i32>().unwrap();
             continue;
         }
 
-        candidates.update(cur);
-        cur = 0;
+        candidates.update(current_count);
+        current_count = 0;
         continue;
     }
 
@@ -52,8 +52,8 @@ fn cal_max_n(lines: Lines, n: usize) -> i32 {
 }
 
 impl Solution for Day1Part1 {
-    fn run(&self, input: &str) -> String {
-        cal_max_n(input.lines(), 1).to_string()
+    fn run(&self, data: Box<dyn Any>) -> String {
+        sum_of_top_n_candidates(input.lines(), 1).to_string()
     }
 }
 
@@ -61,6 +61,6 @@ pub struct Day1Part2 {}
 
 impl Solution for Day1Part2 {
     fn run(&self, input: &str) -> String {
-        cal_max_n(input.lines(), 3).to_string()
+        sum_of_top_n_candidates(input.lines(), 3).to_string()
     }
 }
